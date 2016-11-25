d0ea4e6bb4d8403496cdf0afbeedd66702a5b5f4
David Charte <fdavidcl@protonmail.com>
2016-11-25 19:54:55 +0100

    Añade comentarios

--- sesion1/main.rb
+++ sesion1/main.rb
@@ -1,22 +1,31 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+# Cargamos las dependencias necesarias
 require_relative "newton_raphson"
 require_relative "planet"
 
+# Leemos los argumentos proporcionados por el usuario
 t = begin
       ARGV[0].strip.to_f
     rescue NoMethodError
       0
     end
+
+# Creamos el planeta Tierra con sus parámetros
 tierra = Planet.new(1, 0.017, 365.26)
+
+# Creamos el método de aproximación
 nr = NewtonRaphson.new(
   Math::PI,
+  1e-5,
+  # La función que lleva u en f(u) - ξ
   ->(u) { tierra.f(u) - tierra.ξ(t) },
+  # La función que lleva u en f'(u)
   ->(u) { tierra.df(u) }
 )
 
-ap = nr.approximate 1e-5
+ap = nr.approximate
 puts "Aproximación de u para t=#{t}: #{ap}"
 puts "Posición de la Tierra: #{tierra.position(ap)}"
   
--- sesion1/planet.rb
+++ sesion1/planet.rb
@@ -1,27 +1,42 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+# Clase que representa un planeta, englobando los parámetros que
+# nos permiten calcular su posición en el tiempo
 class Planet
+
+  # Datos miembro
+  #  - a:      semieje mayor de la elipse
+  #  - ε:      módulo del vector de excentricidad
+  #  - period: período de la órbita
   attr_accessor :a, :ε, :period
+  
   def initialize a, ε, period
     self.a = a
     self.ε = ε
     self.period = period
   end
 
+  # Método que calcula la posición del planeta dada la anomalía
+  # excéntrica u
   def position u
     [a * Math::cos(u) - ε, a * Math::sqrt(1 - ε**2) * Math::sin(u)]
   end
 
+  # Método que calcula el escalar independiente de u de la ecuación
+  # de Kepler
   def ξ t
     # Consideramos tiempo inicial t0 = 0
     2 * Math::PI / period * t
   end
 
+  # Método que evalúa la función dependiente de u de la ecuación
+  # de Kepler
   def f u
     u - ε * Math::sin(u)
   end
 
+  # Método que evalúa la derivada de f en u
   def df u
     1 - ε * Math::cos(u)
   end
