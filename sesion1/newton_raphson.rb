2798cd682dbde1681548200101935a07ffdc2188
David Charte <fdavidcl@protonmail.com>
2016-11-25 19:54:27 +0100

    La tolerancia pasa a ser miembro de Approximator

--- sesion1/newton_raphson.rb
+++ sesion1/newton_raphson.rb
@@ -1,46 +1,62 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+# Clase base para métodos numéricos de aproximación de funciones
 class Approximator
-  attr_accessor :current
+
+  # Datos miembro
+  #  - current:   valor de la aproximación actual
+  #  - tolerance: valor de tolerancia
+  attr_accessor :current, :tolerance
   
-  def initialize initial
+  def initialize initial, tolerance
     self.current = initial
+    self.tolerance = tolerance
   end
 
+  # Método a implementar en las clases hijas
   def next_one
     raise NotImplementedError
   end
 
+  # Método que calcula y proporciona aproximaciones
   def each
-    loop do
-      yield current
-      self.current = next_one
-    end
-  end
-
-  def approximate tolerance
     previous = Float::INFINITY
     until (current - previous).abs < tolerance
+      # Proporciona una aproximación y espera a que se
+      # pida la siguiente
+      yield current
       previous = current
       self.current = next_one
     end
-    current
   end
 
+  # Método que devuelve la última aproximación
+  # para la tolerancia dada
+  def approximate
+    to_a.last
+  end
+
+  # Incluimos herramientas que permiten enumerar
+  # las aproximaciones
   include Enumerable
 end
 
 class NewtonRaphson < Approximator
+  # Datos miembro
+  #  - function:   almacena la función f
+  #  - derivative: almacena la derivada de f
   attr_accessor :function, :derivative
   
-  def initialize initial, function, derivative
-    super(initial)
+  def initialize initial, tolerance, function, derivative
+    super(initial, tolerance)
     self.function = function
     self.derivative = derivative
   end
   
   def next_one
+    # Método de Newton-Raphson para encontrar raíces:
+    # Calcula Φ(current) = current - f(current)/f'(current)
     current - function.call(current)/derivative.call(current)
   end
 end
