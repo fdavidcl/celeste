25831c940f3e1b9d0ce570a4a0acfb4941d177ee
David Charte <fdavidcl@protonmail.com>
2016-11-25 20:29:21 +0100

    Añade métodos en Planet para cálculo de anomalía y posición

--- sesion1/main.rb
+++ sesion1/main.rb
@@ -12,22 +12,7 @@ t = begin
       0
     end
 
-# Creamos el planeta Tierra con sus parámetros, calculamos
-# el valor de ξ para el instante t
+# Creamos el planeta Tierra con sus parámetros
 tierra = Planet.new(1, 0.017, 365.26)
-ξ = tierra.ξ(t)
-
-# Creamos el método de aproximación
-nr = NewtonRaphson.new(
-  Math::PI,
-  1e-12,
-  # La función que lleva u en f(u) - ξ
-  ->(u) { tierra.f(u) - ξ },
-  # La función que lleva u en f'(u)
-  ->(u) { tierra.df(u) }
-)
-
-ap = nr.approximate
-puts "Aproximación de u para t=#{t}: #{ap}"
-puts "Posición de la Tierra: #{tierra.position(ap)}"
+puts "Posición de la Tierra: #{tierra.position(t)}"
   
--- sesion1/planet.rb
+++ sesion1/planet.rb
@@ -14,13 +14,31 @@ class Planet
   def initialize a, ε, period
     self.a = a
     self.ε = ε
-    self.period = period
+    self.period = period 
   end
 
   # Método que calcula la posición del planeta dada la anomalía
   # excéntrica u
-  def position u
+  def position_for_excentric u
     [a * Math::cos(u) - ε, a * Math::sqrt(1 - ε**2) * Math::sin(u)]
+  end
+
+  def excentric t
+    # Aproximamos la anomalía excéntrica mediante
+    # Newton-Raphson
+    xi = ξ(t)
+    NewtonRaphson.new(
+      Math::PI,
+      1e-12,
+      # La función que lleva u en f(u) - ξ
+      ->(u) { f(u) - xi },
+      # La función que lleva u en f'(u)
+      ->(u) { df(u) }
+    ).approximate    
+  end
+
+  def position t
+    position_for_excentric excentric(t)
   end
 
   # Método que calcula el escalar independiente de u de la ecuación
