df50a14cb5e4e6842df7a90bc25fb3969df1fe90
David Charte <fdavidcl@protonmail.com>
2016-11-30 16:21:44 +0100

    Implementa aproximador por truncamiento de series

---
+++ sesion1/series.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require_relative "approximator"
+
+class SeriesApproximator < Approximator
+  attr_accessor :term, :n
+  
+  def initialize tolerance = DEFAULT_TOLERANCE, term
+    super(0, tolerance)
+    self.term = term
+    self.n = 0
+  end
+
+  def next_one
+    self.n += 1
+    self.current += term.call(n)
+  end
+end
+
+# Si el archivo es el actual, ejecutamos un ejemplo de
+# aproximación de series
+if __FILE__ == $0
+  # Serie de término n->1/2^n (suma 1)
+  series = SeriesApproximator.new(->(n) { 1.0/2**n })
+  puts series.take(20).to_s
+end
